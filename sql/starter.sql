8567ce96b8d0aa33c6dd7bcebcecbdc9fc0d47ef
timelessnesses <mooping3roblox@gmail.com>
2022-05-11 20:00:57 +0700

    github

--- sql/starter.sql
+++ sql/starter.sql
@@ -12,10 +12,14 @@ CREATE TABLE IF NOT EXISTS logger(
     ruiner_id BIGINT NOT NULL,
     ruined_jump_url TEXT NOT NULL,
     ruined_content TEXT NOT NULL,
-    when_ruined TIMESTAMP NOT NULL
+    when_ruined TIMESTAMP NOT NULL,
+    reason TEXT NOT NULL,
+    previous_chain_id BIGINT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS counting(
     guild_id BIGSERIAL PRIMARY KEY,
-    current_alphabet TEXT NOT NULL,
-    expected_next_alphabet TEXT NOT NULL
+    current_alphabet TEXT,
+    expected_next_alphabet TEXT NOT NULL,
+    previous_alphabet TEXT,
+    previous_person BIGINT
 )
--- src/alphabet_count.py
+++ src/alphabet_count.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from .utils import stuffs
+import datetime
 
 
 class AlphabetCount(commands.Cog):
@@ -22,8 +23,8 @@ class AlphabetCount(commands.Cog):
             view = stuffs.Confirm()
             await ctx.send(
                 embed=discord.Embed(
-                    title="Already setupped",
-                    description="You already setupped this server. This command will override your previous settings. Do you want to do that?",
+                    title="Already set",
+                    description="You have already set up Letter Counting this server. This command will override your previous settings. Do you want to do that?",
                 ),
                 view=view,
             )
@@ -47,38 +48,179 @@ class AlphabetCount(commands.Cog):
                 )
         await ctx.send(
             embed=discord.Embed(
-                title="Alphabet Count Setup",
+                title="Letter Counting Setup",
                 description="""
                 This command will ask you
                 1. Mention a channel
+                2. Can person counting without checking if it is a same person (true/false)
                 """,
                 colour=discord.Colour.green(),
             ),
         )
         ask = [
             "channel",
+            "same_person",
         ]
         answers = {}
         a = None
+        channel = None
         for i in ask:
             a = await ctx.send(
                 embed=discord.Embed(
-                    title="Please Mention {}".format(i), colour=discord.Colour.green()
+                    title="Please answer {}".format(i), colour=discord.Colour.green()
                 )
             )
             answer = await self.bot.wait_for(
                 "message", check=lambda m: m.author == ctx.author
             )
-            if not isinstance(answer, discord.Role):
-                return await ctx.send(
+
+            if i == "channel":
+                answers[i] = answer.id
+                channel = answer
+            else:
+                answers[i] = True if answer.content.lower() == "true" else False
+            await a.delete()
+        await self.db.execute(
+            """
+            INSERT INTO config (guild_id, is_same_person, already_setupped, channel_id)
+            VALUES ($1, $2, $3, $4)
+            """,
+            ctx.guild.id,
+            answers["same_person"],
+            True,
+            answers["channel"],
+        )
+        await ctx.send(
+            embed=discord.Embed(
+                title="Setupped",
+                description=f"""
+                You have setupped this server.
+                Now {channel.mention} will be enforced to count alphabet.
+                """,
+                colour=discord.Colour.green(),
+            )
+        )
+        message = await channel.send(
+            embed=discord.Embed(
+                title="Alphabet Count Rules",
+                description=f"""
+                This channel has been claimed for alphabet count
+                Here's some rules
+                1. {"You can't chain alphabet count else this server will lose streak." if not answers["same_person"] else "You can chain alphabet count."}
+                2. You can't broke counting chain by typing anything other than alphabet else you will lose streak.
+                3. You can't make conversation in this channel else this server will lose streak.
+                """,
+                colour=discord.Colour.green(),
+            )
+        )
+        message2 = await ctx.send(
+            embed=discord.Embed(
+                title="Alphabet Count Howto",
+                description="""
+                Pattern:
+                ```
+                A
+                B
+                ...
+                Z
+                ```
+                But if you reached the end of alphabet, the next format should be something like this
+                ```
+                AA
+                AB
+                ...
+                AZ
+                ```
+                or
+                ```
+                BA
+                BB
+                ...
+                BZ
+                ```
+                If any of you broke the pattern, the server streak will be ended and ended streak will be logged.
+                """,
+                colour=discord.Colour.green(),
+            )
+        )
+        await message.pin()
+        await message2.pin()
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+        if message.guild is None:
+            return
+        if not message.channel.id == await self.db.fetch(
+            "SELECT channel_id FROM config WHERE guild_id = $1", message.guild.id
+        ):
+            return
+        # -------------------------------------------------------
+        # Check if it is a same person
+        previous = await self.bot.fetch_user(
+            int(
+                await self.db.fetch(
+                    "SELECT previous_user_id FROM counting WHERE guild_id = $1",
+                    message.guild.id,
+                )
+            )
+        )
+        if previous is not None:
+            if previous.id == message.author.id:
+                id = stuffs.random_id()
+                now = datetime.datetime.now()
+                await message.channel.send(
                     embed=discord.Embed(
-                        title=f"Please Mention a {i} type properly.",
-                        description="The setup will be stopped.",
+                        title="You can't chain alphabet count",
+                        description=f'Now server\'s chain is ruine by {message.author.mention}. Fallback to "A"',
                         colour=discord.Colour.red(),
                     )
+                    .add_field(name="Ruined ID", value=id)
+                    .add_field(
+                        name="Ruined when", value=now.strftime("%Y/%m/%d %H:%M:%S")
+                    )
+                    .add_field(name="Ruined by", value=message.author.mention)
+                    .add_field(name="Reason", value="Same person")
+                    .add_field(
+                        name="Previous chain",
+                        value=[a async for a in await message.channel.history(limit=1)][
+                            0
+                        ].content,
+                    )
                 )
-            answers[i] = answer.id
-            await a.delete()
-        query = """
-        INSERT INTO alphabet_count (
-        """
+                await self.db.execute(
+                    """
+                    INSERT INTO logger(
+                        id,
+                        guild_id,
+                        channel_id,
+                        ruined_chain_id,
+                        ruiner_id,
+                        ruined_jump_url,
+                        ruined_content,
+                        when_ruined,
+                        reason,
+                        previous_chain_id,
+
+                    )
+                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+                    """,
+                    id,
+                    message.guild.id,
+                    message.channel.id,
+                    message.id,
+                    message.author.id,
+                    message.jump_url,
+                    message.content,
+                    now,
+                    "Same person",
+                    [a async for a in await message.channel.history(limit=1)][
+                        0
+                    ].content,
+                )
+                await self.db.execute(
+                    """
+                    UPDATE counting SET previous_user_id = NULL WHERE guild_id = $1
+                    """
+                )
