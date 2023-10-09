df2e43bcdb3d9e6a8ddedecebf14eb304d491356
timelessnesses <timelessnesses@timelessnesses.me>
2023-10-09 17:17:28 +0700

    clean

--- backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Get a list of all databases
-databases=$(psql -t -c "SELECT datname FROM pg_database WHERE datistemplate = false;")
-# Loop through each database and backup to a file
-for db in $databases; do pg_dump $db > /root/backup/$db.sql
-done
